cecbdd85cb8680e816af9a0a9291317bbd27d358
Daikin2001 <ltdat2001alt@gmail.com>
2019-03-06 16:35:24 +0700

    up

--- ĐKRN-P1/main.js
+++ ĐKRN-P1/main.js
@@ -25,8 +25,20 @@ function lapPhuong(a){
 document.write(lapPhuong(5));
 document.write("<br>");
 
-function tinhTongBinhPhuongcua2So(a,b){
-    return (a*a)+(b*b)
+function tongBinhPhuongcua2So(a,b){
+    return (a*a)+(b*b);
 }
-document.write(tinhTongBinhPhuongcua2So(5,6));
+document.write(tongBinhPhuongcua2So(5,6));
+document.write("<br>");
+
+function tongLapPhuong2so(a,b){
+    return (a*a*a)+(b*b*b);
+}
+document.write(tongLapPhuong2so(5,6));
+document.write("<br>");
+
+function binhPhuongCuaTong2so(a,b){
+    return (a+b)*2;
+}
+document.write(binhPhuongCuaTong2so(5,2));
 document.write("<br>");
