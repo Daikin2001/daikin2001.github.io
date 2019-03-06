deb0e5c114619e529324e9eaf0f7bf0826a61b08
Daikin2001 <ltdat2001alt@gmail.com>
2019-03-06 15:46:47 +0700

    update

--- ĐKRN-P1/main.js
+++ ĐKRN-P1/main.js
@@ -1,8 +1,25 @@
 // Điều kiện rẽ nhánh - Phần 1.
-// Bài 3:
-function bai1(arr){
-    for (let i = 0; i < arr.length; i++) {
-        const element = arr[i];
-        
+// Bài 1:
+function bai1(a,b){
+    if(typeof a != "number" || typeof b != "number"){
+        return "Không phải là số";
+    }
+    if (a>b){
+        return a;
+    }else{
+        return b;
     }
 }
+document.write(bai1("1","2"));
+document.write("<br>");
+
+function tinhTong(a,b){
+    return a + b;
+}
+document.write(tinhTong(3,5));
+document.write("<br>");
+
+function lapPhuong(a){
+    return a*a;
+}
+document.write(lapPhuong(5));
