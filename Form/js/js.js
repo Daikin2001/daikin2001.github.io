ba3a73eb4dc33e81cda0af8cc09d2dcc3c412217
Daikin2001 <ltdat2001alt@gmail.com>
2019-05-18 17:11:16 +0700

    update

--- Form/js/js.js
+++ Form/js/js.js
@@ -8,11 +8,18 @@ for (var i=0;i<parameters.length;i++){
     var temp;
     temp=parameters[i].slice(index+1);
     // dataForm[i]=temp.replace(/+/g,"*");
-    dataForm[i]=decodeURIComponent(temp.split('?').join(' '));
+    dataForm[i]=decodeURIComponent(temp.split('+').join(' '));
 }
 var register = {
     userName:dataForm[0],        
     email:dataForm[1],        
     pass:dataForm[2],
-    confirmpassword:dataForm[3]
 };
+
+// const URL = decodeURIComponent(window.location.href);
+// const data=[];
+
+// for (let i = 0; i < data.length; i++) {
+//     data[i] = data[i].split('=');
+//     data[i][0] = '<b>' + data[i][0] + '</b>';
+// }
