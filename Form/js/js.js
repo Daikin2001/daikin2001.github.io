2ab419b04470cd22ab5448095ea7a3e6698c45c8
Daikin2001 <ltdat2001alt@gmail.com>
2019-05-19 10:58:27 +0700

    update

--- Form/js/js.js
+++ Form/js/js.js
@@ -23,3 +23,22 @@ var register = {
 //     data[i] = data[i].split('=');
 //     data[i][0] = '<b>' + data[i][0] + '</b>';
 // }
+
+$(document).ready(function() {
+    
+    //Khi bàn phím được nhấn và thả ra thì sẽ chạy phương thức này
+    $("#formRegister").validate({
+        rules: {
+            username: "required",
+            email: "required",
+            password: "required",
+            confirm_password: "required"
+        },
+        messages: {
+            username: "Please enter username",
+            email: "Please enter email",
+            password: "Please enter password",
+            confirm_password: "Please enter confirm password"
+        }
+    });
+});
